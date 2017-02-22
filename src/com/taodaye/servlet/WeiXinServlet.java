1e15d6bd9100c0fc57fef12b6fc7bb55e45313d6
wbwuwb <wbwuwb@cn.ibm.com>
2017-02-22 10:33:11 +0800

    setup weixin POC

--- src/com/taodaye/servlet/WeiXinServlet.java
+++ src/com/taodaye/servlet/WeiXinServlet.java
@@ -2,13 +2,20 @@ package com.taodaye.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.dom4j.DocumentException;
+
+import com.taodaye.entity.TextMessage;
 import com.taodaye.util.CheckUtil;
+import com.taodaye.util.MessageUtil;
+import com.thoughtworks.xstream.converters.basic.DateConverter;
 
 public class WeiXinServlet extends HttpServlet{
 
@@ -29,7 +36,43 @@ public class WeiXinServlet extends HttpServlet{
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doPost(req, resp);
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		System.out.println(new Date().getTime());
+		PrintWriter out  = resp.getWriter();
+		Map<String, String> map;
+		try {
+			map = MessageUtil.xmlToMap(req);
+			String fromUserName = map.get("FromUserName");
+			String toUserName = map.get("ToUserName");
+			String msgType = map.get("MsgType");
+			String content = map.get("Content");
+			String msgId = map.get("MsgId");
+			String message = null;
+			if("text".equals(msgType)){
+				TextMessage textMessage = new TextMessage();
+				textMessage.setFromUserName(fromUserName);
+				textMessage.setToUserName(toUserName);
+				textMessage.setMsgType("text");
+				long currentTime = new Date().getTime();
+				textMessage.setCreateTime(currentTime+"");
+				textMessage.setMsgId(msgId);
+				textMessage.setContent("The message you sent is: "+content);
+				message = MessageUtil.textMessageToXML(textMessage);
+				}
+			System.out.print(message);
+			out.print(message);
+			out.flush();
+			System.out.println(new Date().getTime());
+		} catch (DocumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally{
+			
+			out.close();
+		}
+		
+		
 	}
 
 	
---
+++ src/com/taodaye/util/MessageUtil.java
@@ -0,0 +1,58 @@
+package com.taodaye.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
+
+import com.taodaye.entity.TextMessage;
+import com.thoughtworks.xstream.XStream;
+
+public class MessageUtil {
+
+	/**
+	 * xml transfer to map
+	 * @param req
+	 * @return
+	 * @throws IOException
+	 * @throws DocumentException
+	 */
+	public static Map<String,String> xmlToMap(HttpServletRequest req) throws IOException, DocumentException{
+		Map<String,String> map = new HashMap<String,String>();
+		SAXReader reader = new SAXReader();
+		InputStream ins = req.getInputStream();
+		Document doc = reader.read(ins);
+		
+		Element root = doc.getRootElement();
+		
+		List<Element> list = root.elements();
+		
+		for(Element e:list){
+			map.put(e.getName(), e.getText());
+		}
+		
+		ins.close();
+		return map;
+	}
+	
+	/**
+	 * 
+	 * transfer Object to XML
+	 * @param textMessage
+	 * @return
+	 */
+	public static String textMessageToXML(TextMessage textMessage){
+		XStream xstream = new XStream();
+		xstream.alias("xml", textMessage.getClass());
+		return xstream.toXML(textMessage);
+		
+	}
+}
